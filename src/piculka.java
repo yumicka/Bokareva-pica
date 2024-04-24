97696ef7b06bc668da1086c54a1bc80e2146bc70
Alina Bokareva <alina.bokareva@sk.lvt.lv>
2024-04-24 10:17:51 +0300

    Pielaboju lietotaja informacija ievadišanu

--- src/piculka.java
+++ src/piculka.java
@@ -5,8 +5,7 @@ public class piculka {
 	public static void main(String[] args) {
 		//https://stock.adobe.com/search?k=pizza+png
 		
-		String izvele, vards, persvards, adrese;
-		int talrun;
+		String izvele, vards, persvards, adrese, telefons;
 		String[] persona = {"Pircejs", "Personāls"};
 		
 		String[] darbibas = {"Menu", "Iegadies picu", "Mainit savu info", "Iziet no programmas"};
@@ -17,9 +16,34 @@ public class piculka {
 				null, persona, persona[0]);
 		
 		if(izvele == "Pircejs") {
+			do {
 			vards = (String)JOptionPane.showInputDialog(null, "Jūsu vārds: ");
-			adrese = (String) JOptionPane.showInputDialog(null, "Ievādies savu adresi: ");
-			talrun = Integer.parseInt(JOptionPane.showInputDialog("Ievadiet talruni:"));
+			}while(vards.length() <3);
+			
+			boolean pareizi = false;
+			do {
+				adrese = (String) JOptionPane.showInputDialog(null, "Ievādies savu adresi:\n(... iela ...-...)");
+				if (adrese.matches(".* iela \\d+-\\d+"))
+					pareizi = true;
+				else {
+					JOptionPane.showMessageDialog(null, "Adrese ievadita nekorekti!"
+							+ "\nPareizais variants: {ielas nosaukums} iela {ielas numurs} - {dzivoklis}");
+					pareizi = false;
+				}
+			}while(!pareizi);
+			
+			pareizi = false;
+			do {
+				telefons = (String)JOptionPane.showInputDialog("Ievadiet talruni: (bez +371)");
+				if (telefons.matches("2\\d{7}"))
+					pareizi = true;
+				else {
+					JOptionPane.showMessageDialog(null, "Telefona nurums ievadts nekorekti!"
+							+ "\nTelefona numura nevar būt +371 un mazāk par 8 simboliem");
+				}
+				
+			}while(!pareizi);
+			int talrun = Integer.parseInt(telefons);
 			Pircejs pirc = new Pircejs(vards, adrese, talrun);
 			do {
 			izvele = (String)JOptionPane.showInputDialog(null, "Izveleties funkciju:", "Izvele",
@@ -45,7 +69,7 @@ public class piculka {
 			
 		}else if(izvele == "Personāls") {
 			persvards = (String)JOptionPane.showInputDialog(null, "Ievadiet savu vardu: ");
-			//šeit būs fails ar vardiem, kuram pieļauti izmantot personala funkcijas un salidzinašana
+			//šeit būs fails ar vardiem, kuram pieļauti izmantot personala funkcijas un salidzinašana)
 		}
 		
 		
